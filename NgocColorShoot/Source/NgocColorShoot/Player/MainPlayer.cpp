dee24c77e4c4f41f097181801adb36006e8f8664
Ngoc Le <le000222@algonquinlive.com>
2024-03-27 18:05:15 -0400

    Adding Base code and make the player moves

--- NgocColorShoot/Source/NgocColorShoot/Player/MainPlayer.cpp
+++ NgocColorShoot/Source/NgocColorShoot/Player/MainPlayer.cpp
@@ -2,6 +2,11 @@
 
 
 #include "MainPlayer.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "../Weapon/Weapon.h"
 
 // Sets default values
 AMainPlayer::AMainPlayer()
@@ -9,7 +14,133 @@ AMainPlayer::AMainPlayer()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ColorShoot = CreateDefaultSubobject<UBoxComponent>(TEXT("ColorShoot"));
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualMesh"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	MainCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	FloatingPawnMovement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Movement"));
+
+	ColorShoot->SetSimulatePhysics(true);
+	ColorShoot->SetEnableGravity(true);
+	ColorShoot->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+	ColorShoot->SetCollisionProfileName("Pawn");
+	ColorShoot->SetLinearDamping(10);
+
+	SetRootComponent(ColorShoot);
+	SpringArm->SetupAttachment(RootComponent);
+	VisualMesh->SetupAttachment(RootComponent);
+	MainCamera->SetupAttachment(SpringArm);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	VisualMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	VisualMesh->SetCollisionProfileName(TEXT("IgnoreAll"));
+
+	CurrentWeapon = nullptr;
+	CurrentHealth = MaximumHealth;
+
+	bIsAiming = false;
+	bIsFiring = false;
+	bPaused = false;
 }
+
+void AMainPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
+void AMainPlayer::HoldWeapon(AWeapon* Weapon)
+{
+	check(Weapon != nullptr && "Passed a null weapon!");
+	//TODO Lab3 ACharacterBase::HoldWeapon(...):
+	/* Drop currently carried weapon first.*/
+	//CALL DropWeapon()
+	DropWeapon();
+
+	/* Attach weapon to the character.*/
+	//SET CurrentWeapon to weapon
+	CurrentWeapon = Weapon;
+	//CALL Attach() on the CurrentWeapon and pass in this
+	//CurrentWeapon->Attach(this);
+
+	//CALL Clear() on the CurrentWeapon's OnWeaponFired event
+	//CurrentWeapon->OnWeaponFired.Clear();
+	/* Subscribe to weapon's events.*/
+	//SUBSCRIBE to the CurrentWeapon's OnWeaponFired and pass in (this, &ACharacterBase::OnWeaponFired)
+	//CurrentWeapon->OnWeaponFired.AddDynamic(this, &ACharacterBase::OnWeaponFired);
+}
+
+void AMainPlayer::DropWeapon()
+{
+	if (CurrentWeapon != NULL)
+	{
+		/* Unsubscribe from weapon's events.*/
+		//CALL RemoveDynamic(this, &ACharacterBase::OnWeaponFired) on the CurrentWeapon's OnWeaponFired event
+		//CurrentWeapon->OnWeaponFired.RemoveDynamic(this, &ACharacterBase::OnWeaponFired);
+
+		/* Detach weapon from the character.*/
+		//CALL Detach() on the CurrentWeapon()
+		//CurrentWeapon->Detach();
+		//SET CurrentWeapon to null
+		CurrentWeapon = nullptr;
+
+		/* Reset weapon states states.*/
+		//SET bIsFiring to false
+		bIsFiring = false;
+		//SET bIsAiming to false
+		bIsAiming = false;
+	}
+}
+
+void AMainPlayer::MoveRight(float Amount)
+{
+	if (!bPaused)
+		FloatingPawnMovement->AddInputVector(GetActorRightVector() * Amount);
+}
+
+void AMainPlayer::MoveForward(float Amount)
+{
+	if (!bPaused)
+		FloatingPawnMovement->AddInputVector(GetActorForwardVector() * Amount);
+}
+
+void AMainPlayer::Turn(float Amount)
+{
+	if (!bPaused)
+		AddControllerYawInput(Amount);
+}
+
+void AMainPlayer::Pause()
+{
+	//UGameplayStatics::OpenLevel(GetWorld(), FName(TEXT("MainMenu")));
+	bPaused = !bPaused;
+
+}
+
+//void AMainPlayer::Fire(bool Toggle)
+//{
+//	if (CurrentWeapon)
+//	{
+//		if (Toggle)
+//		{
+//			CurrentWeapon->PullTrigger();
+//		}
+//		else
+//		{
+//			CurrentWeapon->ReleaseTrigger();
+//		}
+//
+//		//SET bIsFiring to Toggle
+//		bIsFiring = Toggle;
+//	}
+//}
+
+//void AMainPlayer::Aim(bool Toggle)
+//{
+//	bIsAiming = Toggle;
+//}
 
 // Called when the game starts or when spawned
 void AMainPlayer::BeginPlay()
@@ -18,11 +149,8 @@ void AMainPlayer::BeginPlay()
 	
 }
 
-// Called every frame
-void AMainPlayer::Tick(float DeltaTime)
+void AMainPlayer::OnHitActor(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit)
 {
-	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -30,5 +158,15 @@ void AMainPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainPlayer::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainPlayer::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &AMainPlayer::Turn);
+	PlayerInputComponent->BindAction("Pause", IE_Pressed, this, &AMainPlayer::Pause);
+}
+
+
+AWeapon* AMainPlayer::GetCurrentWeapon()
+{
+	return CurrentWeapon;
 }
 
--- NgocColorShoot/Source/NgocColorShoot/Player/MainPlayer.h
+++ NgocColorShoot/Source/NgocColorShoot/Player/MainPlayer.h
@@ -15,9 +15,50 @@ public:
 	// Sets default values for this pawn's properties
 	AMainPlayer();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "ColorShoot")
+	class UBoxComponent* ColorShoot;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "ColorShoot")
+	UStaticMeshComponent* VisualMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Camera")
+	class UCameraComponent* MainCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Camera")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(EditDefaultsOnly)
+	float MaxSpeed;
+
+	UFUNCTION(BlueprintPure)
+	class AWeapon* GetCurrentWeapon();
+
+	UPROPERTY(EditDefaultsOnly)
+	float MaximumHealth = 40.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	float CurrentHealth;
+
+	//UPROPERTY(EditAnywhere, Category = "HUD");
+	//TSubclassOf<class UPongHUD> WBP_PongHUD;
+
+	//class UPongHUD* MyHud;
+
+	// Function to fire the weapon.
+	//void Fire(bool Toggle);
+	// Function to toggle aiming mode.
+	//void Aim(bool Toggle);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	class UFloatingPawnMovement* FloatingPawnMovement;
+
+	float CurrentVelocity = 0;
+
+	UFUNCTION()
+	void OnHitActor(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit);
 
 public:	
 	// Called every frame
@@ -26,4 +67,29 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	float MoveVerticalAmount = 0;
+	float MoveHorizontalAmount = 0;
+
+	void HoldWeapon(class AWeapon* Weapon);
+	// Function to drop the currently held weapon.
+	void DropWeapon();
+
+	void Turn(float Amount);
+	void MoveForward(float Amount);
+	void MoveRight(float Amount);
+	void Pause();
+
+	UPROPERTY()
+	class AWeapon* CurrentWeapon;
+
+	// Boolean flag to indicate aiming state.
+	bool bIsAiming;
+	// Boolean flag to indicate firing state.
+	bool bIsFiring;
+
+	// Boolean flag to indicate if the character has a weapon.
+	bool bHasWeapon;
+
+	bool bPaused;
 };
